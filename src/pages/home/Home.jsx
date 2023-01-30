0537306c96f761b73fc04c3c88da87900f37c9f2
sat770 <109108471+sat770@users.noreply.github.com>
2023-01-30 10:03:09 +0200

    home and footer

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,37 @@
 import React from 'react'
+import "./home.css"
+import imgLogo from "../../Assets/IMG-20230126-WA0018-removebg-preview (1).png"
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <>
+    <div className="home-page">
+      <div className="top">
+        <div className="logo">
+        <img className='logo-size' src={imgLogo} alt="logo" />
+        </div>
+        <div className="account">
+          <button>
+          <span className='material-symbols-outlined size-icon'>account_circle</span>
+          </button>
+        </div>
+      </div>
+      <div className="bady">
+        <div className="search">
+          <input className='font-input' type="text" placeholder={'Enter Address Here...'}/>
+          <button>
+              <span class="material-symbols-outlined">
+                  search
+              </span>
+          </button>
+        </div>
+        <div className="button-search">
+        <button className='stail-buttom test'>search by address</button>
+        <button className='stail-buttom'>search by location</button>
+        </div>
+      </div>
+    </div>
+
+    </>
   )
 }
