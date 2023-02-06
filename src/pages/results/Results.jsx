408e1c723f3797625134df68716db5d07983ceed
sat770 <109108471+sat770@users.noreply.github.com>
2023-02-06 12:49:40 +0200

    parkings start

--- src/pages/results/Results.jsx
+++ src/pages/results/Results.jsx
@@ -1,11 +1,11 @@
-import { useEffect } from "react";
-import { db } from "../../firebaseConfig";
+import { useEffect,useState } from "react";
+import { db, auth } from "../../firebaseConfig";
 import {
   collection,
   onSnapshot,
   addDoc,
   deleteDoc,
-  doc as doc2,
+  doc,
   query,
   where,
   orderBy,
@@ -14,41 +14,54 @@ import {
   setDoc,
   getDocs,
 } from "firebase/firestore";
+import tempImage from "../../Assets/logo344.png";
+import ParkingCard from "../../components/parkingCard/ParkingCard";
 
 export default function Results({ selectedAddress }) {
   if (Object.keys(selectedAddress).length !== 0) {
-    localStorage.setItem("selectedAddress",JSON.stringify(selectedAddress))
-  }
-  else {
-    selectedAddress = JSON.parse(localStorage.selectedAddress)
+    localStorage.setItem("selectedAddress", JSON.stringify(selectedAddress));
+  } else {
+    selectedAddress = JSON.parse(localStorage.selectedAddress);
   }
 
+  const [parkings, setParkings] = useState([])
   const parkingsRef = collection(db, "parkings");
-  console.log(selectedAddress);
-  let parkings = [];
-  async function func() {
-    const querySnapshot = await getDocs(parkingsRef);
-    querySnapshot.forEach((doc) => {
-      // doc.data() is never undefined for query doc snapshots
-      console.log(doc.id, " => ", doc.data());
-      deleteDoc(doc2(parkingsRef, doc.id))
-        .then((e) => console.log(e))
-        .catch((e) => console.log(e));
+  // console.log(selectedAddress);
+  useEffect(() => {
+    getDocs(parkingsRef).then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        console.log(doc.id, ":", doc.data());
+        setParkings([...parkings, doc])
+      });
     });
-  }
+  }, [])
   
+  // updateDoc(doc(db,"parkings","V2UpAParn0nasB02v0de"),{
+  //   ownerId:auth && auth.currentUser?auth.currentUser.uid:null
+  // })
 
-  async function addParking() {
-    await addDoc(parkingsRef, {
-      address: selectedAddress,
-      roofed: false,
-      electricCharger: false,
-      available: true,
-      boring: "i guess",
-      bla: "yes",
-    });
-  }
+  // async function addParking() {
+  //   await addDoc(parkingsRef, {
+  //     address: selectedAddress,
+  //     roofed: false,
+  //     electricCharger: false,
+  //     available: true,
+  //     boring: "i guess",
+  //     bla: "yes",
+  //   });
+  // }
 
   // const q = query(parkingsRef, where("id", "==", "current user id"), orderBy('some order(key in item  (optional)' ))
-  return <div >Results</div>;
+  // console.log("parkings", parkings);
+  // if (parkings.length !== 0) console.log(parkings);
+  return (
+    <div>
+      {parkings && parkings.map((parking) => {
+        
+          console.log("h");
+          return <ParkingCard details={parking} />;
+        
+      })}
+    </div>
+  );
 }
